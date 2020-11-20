3a3bc93cc9aaf1c7e0c90925fadbc5b71818908c
Rowan Molony <rowan.molony@codema.ie>
2020-11-20 14:05:53 +0000

    Redo order of venti func
    Somehow this was missed in last bug fix...

--- src/seai_deap/vent.py
+++ src/seai_deap/vent.py
@@ -1,19 +1,21 @@
+from typing import Optional
+
 import numpy as np
 
 
 def calculate_ventilation_heat_loss(
     volume: np.array,
-    ventilation_heat_loss_constant: float = 0.33,
-    effective_air_rate_change: float = 0.5,
+    ventilation_heat_loss_constant: Optional[float] = 0.33,
+    effective_air_rate_change: Optional[float] = 0.5,
 ) -> np.array:
     """Calculate ventilation heat loss (DEAP 4.2.2 Vent!G34)
 
     Args:
         volume (np.array): m^3
-        ventilation_heat_loss_constant (float, optional): [description]. Defaults to 0.33.
-        effective_air_rate_change (float, optional): [description]. Defaults to 0.5.
+        ventilation_heat_loss_constant (float, optional): DEAP assumption. Defaults to 0.33.
+        effective_air_rate_change (float, optional): DEAP assumption. Defaults to 0.5.
 
     Returns:
         np.array: W/K
     """
-    return ventilation_heat_loss_constant * effective_air_change_rate * volume
+    return ventilation_heat_loss_constant * effective_air_rate_change * volume
